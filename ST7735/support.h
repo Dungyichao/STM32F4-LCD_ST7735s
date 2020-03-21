fe17edd0ec0dcf325576d5ee2bb869cc0f7a9af8
Dung-Yi <dungyichao@gmail.com>
2020-03-21 15:12:41 -0400

    Add files via upload
    https://www.geeksforgeeks.org/convert-floating-point-number-string/

---
+++ ST7735/support.h
@@ -0,0 +1,8 @@
+#ifndef __support
+#define __support
+
+#include <math.h>
+void ftoa(float n, char* res, int afterpoint) ;  // floating/double to string (display on LCD usage)
+int intToStr(int x, char str[], int d) ;
+
+#endif
